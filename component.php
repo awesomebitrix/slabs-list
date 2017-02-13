dd68009d0f27756da96cd5d5f7247115cc81724e
Роман Г <qscary@yandex.ru>
2017-02-13 18:13:33 +0500

    restructure

--- component.php
+++ component.php
@@ -1,65 +1,93 @@
 <?if(!defined("B_PROLOG_INCLUDED") || B_PROLOG_INCLUDED!==true)die();
 
-use Bitrix\Main\Loader,
-    Bitrix\Main\Data\Cache;
+use Bitrix\Main\Loader;
 
 if( Loader::includeModule('iblock') === false ) die();
 
-$cache_id = md5(serialize($arParams));
-$cache_dir = "/slabs";
+if( $this->StartResultCache() ) {
 
-$obCache = Cache::createInstance();
+    $arResult = [];
 
-if($obCache->InitCache(36000, $cache_id, $cache_dir)) {
-    $arResult = $obCache->GetVars();
-}elseif($obCache->StartDataCache()) {
-    global $CACHE_MANAGER;
+    $arFields = [
+        "ID",
+        "NAME",
+        "IBLOCK_ID",
+    ];
+
+    $arFilter =[
+        "IBLOCK_TYPE"   => $arParams["IBLOCK_TYPE"],
+        "IBLOCK_ID"     => $arParams["IBLOCK_ID"],
+        "ACTIVE"        => "Y",
+    ];
+
+    if( is_array($arParams["FIELDS"]) )
+        $arFields = array_merge($arFields,$arParams["FIELDS"]);
+
+    if( is_array($arParams["PROPERTIES"]) )
+        foreach ($arParams["PROPERTIES"] as $property)
+            $arFields[] = "PROPERTY_" . $property;
 
     $res = CIBlockElement::GetList(
         [],
-        [
-            "IBLOCK_TYPE"   => $arParams["IBLOCK_TYPE"],
-            "IBLOCK_ID"     => $arParams["IBLOCK_ID"],
-            "ACTIVE"        => "Y",
-        ],
+        $arFilter
+        ,
         false,
         false,
-        [
-            "ID",
-            "NAME",
-            "PREVIEW_PICTURE",
-        ]
+        $arFields
     );
 
-    $CACHE_MANAGER->StartTagCache($cache_dir);
+    unset($property);
 
-    while ( $arElement = $res->Fetch() ) {
-
-        $CACHE_MANAGER->RegisterTag("iblock_id_".$arElement["IBLOCK_ID"]);
+    while ($arElement = $res->Fetch() ){
 
         $arButtons = CIBlock::GetPanelButtons(
-            $arParams["IBLOCK_ID"],
+            $arElement["IBLOCK_ID"],
             $arElement["ID"],
             0,
             array("SECTION_BUTTONS"=>false, "SESSID"=>false)
         );
 
-        $arResult["ITEMS"][] = [
+        if( is_array($arParams["FIELDS"]) ){
+            foreach ($arParams["FIELDS"] as $fields)
+                if (strpos($fields, "PICTURE") === false)
+                    $arFieldsResult[$fields] = $arElement[$fields];
+                else
+                    $arFieldsResult[$fields] = CFile::GetFileArray($arElement[$fields]);
+        }else
+            foreach ($arFields as $fields)
+                $arFieldsResult[$fields] = $arElement[$fields];
+
+        unset($fields);
+
+        if( is_array($arParams["PROPERTIES"]) ) {
+            foreach ($arParams["PROPERTIES"] as $property){
+                if ( strpos($property, "PICTURE") === false && strpos($property, "GALLERY") == false) {
+                    if( is_array($arElement["PROPERTY_".$property."_VALUE"]) )
+                        foreach ($arElement["PROPERTY_".$property."_VALUE"] as $value)
+                            $arFieldsResult[$property][] = $value;
+                    else
+                        $arFieldsResult[$property] = $arElement["PROPERTY_".$property."_VALUE"];
+                }else{
+                    if( is_array($arElement["PROPERTY_".$property."_VALUE"]) )
+                        foreach ($arElement["PROPERTY_".$property."_VALUE"] as $idPicture)
+                            $arFieldsResult[$property][] = CFile::GetFileArray($idPicture);
+                    else
+                        $arFieldsResult[$property] = CFile::GetFileArray($arElement["PROPERTY_".$property."_VALUE"]);
+                }
+            }
+        }
+
+        $arItem = [
             "ID"            => $arElement["ID"],
             "NAME"          => $arElement["NAME"],
-            "PICTURE"       => CFile::GetFileArray($arElement["PREVIEW_PICTURE"])["SRC"],
+            "IBLOCK_ID"     => $arElement["IBLOCK_ID"],
             "EDIT_LINK"     => $arButtons["edit"]["edit_element"]["ACTION_URL"],
             "DELETE_LINK"   => $arButtons["edit"]["delete_element"]["ACTION_URL"],
         ];
+
+        $arResult["ITEMS"][] = array_merge($arItem,$arFieldsResult);
+        unset($arItem,$arFieldsResult);
     }
 
-    $CACHE_MANAGER->RegisterTag("iblock_id_new");
-    $CACHE_MANAGER->EndTagCache();
-
-    $obCache->EndDataCache($arResult);
-}else {
-    $arResult = [];
+    $this->IncludeComponentTemplate($componentPage);
 }
-
-
-$this->IncludeComponentTemplate($componentPage);
